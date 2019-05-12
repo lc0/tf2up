0dca9e95ae460ac041ed507d8c3ebbf6cf64889f
Sergii Khomenko <khomenko@brainscode.com>
2019-05-12 20:35:32 +0200

    Add a first draft to store file to GCS

---
+++ src/storage.py
@@ -0,0 +1,74 @@
+from google.cloud import bigquery, storage
+
+# TODO: move this to config
+CLOUD_STORAGE_BUCKET = 'tf2up'
+
+class Storage(object):
+
+    def __init__(self, dataset_id: str, table_id: str, dataset_location='US'):
+        # TODO: how do we pass credentials file
+        self.dataset_id = dataset_id
+        self.table_id = table_id
+        self.dataset_location = dataset_location
+
+        self._auth()
+
+    def _auth(self) -> None:
+        """Auth with credentials provided via GOOGLE_APPLICATION_CREDENTIALS"""
+
+        self._bq_client = bigquery.Client()
+        self._gs_client = storage.Client(project=self.dataset_id)
+
+    @staticmethod
+    def gen_filename(filename) -> str:
+        """Generate a filename for GCS"""
+
+        return filename
+
+    def save_file(self, filename):
+        """Store file to GCS"""
+
+        remote_filename = Storage.gen_filename(filename)
+
+        bucket = self._gs_client.bucket(CLOUD_STORAGE_BUCKET)
+        blob = bucket.blob(remote_filename)
+
+        blob.upload_from_filename(filename)
+
+        # TODO: do we need explicitly specify
+        # upload_from_string to upload from bytestream
+        # blob.upload_from_string(file_stream, content_type=content_type)
+
+
+    def _normalize_bq_file(self, file):
+        """
+        Transform a raw file to be able to store in a reasonable format in BQ
+        """
+
+        pass
+
+    def save_to_bq(self, filename,
+                  source_format=bigquery.SourceFormat.CSV):
+        dataset_ref = self._bq_client.dataset(self.dataset_id)
+        table_ref = dataset_ref.table(self.table_id)
+
+        job_config = bigquery.LoadJobConfig()
+        job_config.source_format = source_format
+        job_config.skip_leading_rows = 1
+        job_config.autodetect = True
+
+        with open(filename, "rb") as source_file:
+            job = self._bq_client.load_table_from_file(
+                source_file,
+                table_ref,
+                location=self.dataset_location,
+                job_config=job_config,
+            )
+
+        job.result()  # Waits for table load to complete.
+
+
+if __name__ == "__main__":
+    storage = Storage(dataset_id='foo', table_id='bar')
+    storage.save_file('/srv/labs.brainscode/tf2up/cluster_setup/helm_init.sh')
+    print("All amazing so far")
